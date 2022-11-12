9cee754db74c1b0115ec30c24787d149b7076f78
Uday Singh <usingh5@sapient.com>
2022-11-12 19:39:58 +0530

    Updating configs

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ module.exports = {
   CSV_INPUT_FILE: "ks-na-master-catalog-sur.csv",
   XML_OUTPUT_FILE: "./output/inventory-lists/KS-NA-Surprise-DC-All-Stores-Inventory.xml",
   INVENTORY_LIST_ID: "KS-NA-Surprise-DC-All-Stores-Inventory",
-  STOREFRONT_HOSTNAME : "staging-test-tapestryinc.demandware.net",
+  STOREFRONT_HOSTNAME : "XXXXXXX.net",
   OUTPUT_FOLER:'./output/',
   ARCHIEVE_NAME:'output.zip',
-  CLIENT_ID:'fd922ec7-e515-47fb-958c-2366f60a80f7',
-  CLIENT_SECRET:'TapestryJenkin@123'
+  CLIENT_ID:'CLIENT_ID',
+  CLIENT_SECRET:'SECRET'
 };
